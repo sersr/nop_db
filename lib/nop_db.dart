1bc9385f6c84ad0c2cd63964ab545c2778036894
sersr <1334349436@qq.com>
2022-11-20 21:19:38 +0800

    更新 约束

--- lib/nop_db.dart
+++ lib/nop_db.dart
@@ -1,7 +1,5 @@
 library nop_db;
 
-export 'package:nop_annotations/nop_annotations.dart';
-
 export 'src/database/database.dart';
 export 'src/database/nop_db.dart';
 export 'src/database/table.dart';
